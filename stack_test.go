20ea28dfb4a80b60ca8ec5deee004e1ce15782bb
0x7fffffff <uncharted_24@foxmail.com>
2022-01-23 00:48:21 +0800

    update TestStack

--- stack_test.go
+++ stack_test.go
@@ -8,17 +8,10 @@ import (
 func TestStack(t *testing.T) {
 	s := NewStack()
     	s.Push(233, 666, 1024)
-	PrintStackInfo(s)
+	s.Print()
 	for !s.Empty() {
 		s.Pop()
 	}
 	s.Pop()
-	PrintStackInfo(s)
-}
-
-func PrintStackInfo(s *Stack){
-	fmt.Println("stack len is", s.Len())
-    	fmt.Println("stack empty is", s.Empty())
-	fmt.Println("stack top is", s.Top())
 	s.Print()
 }
